cc902b8104b02aa16e78809a88452a7b615db60e
builtwebtough <builtwebtough@gmail.com>
2023-09-22 09:13:39 -0700

    updated main.js added w3.css and time tracker settings file

---
+++ main.js
@@ -0,0 +1,3 @@
+/* testing the time tracker to see if it pauses after 30 seconds */
+/* and it works and it automatically picks back up when i start again */
+/* need to be carefull about hitting the start button on this thing */
