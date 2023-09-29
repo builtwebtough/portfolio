cbee3c0c5ea346b50ec4ec2b48debc7be0245806
builtwebtough <builtwebtough@gmail.com>
2023-09-29 11:03:57 -0700

    added comments to main.php to help direct the direction im going with it

--- main.php
+++ main.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <?php
-if(isset($_POST['login'])){ echo "<h2>This came from the login page</h2>";
+//This page will process all form requests
+if(isset($_POST['login'])){ 
+    //we shall handle the login request here and redirect to an account php page that will display 
+    //content relative to the account which is logged in.
+    echo "<h2>This came from the login page</h2>";
 }elseif(isset($_POST['signup'])){
-echo "<h2>This came from the contact form</h2>";
+    //we shall post this form to a database table and send
+    // an email to the admin email notifiying of new account request.
+    //and possibly email the requester via their submitted email address
+    //then send the user to a comfirmation page that after a few seconds
+    //redirects back to the main page.
+    echo "<h2>This came from the contact form</h2>";
 }else{
-echo "<h2>This is where we handle all php post from the account page and the contact form<br>
-we will load various pages from this code.
-</h2>";
+   //handle errors on submission here.
 }
 ?> 
 
