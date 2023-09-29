d1fe54d3c851d28e4f146012aa91c33f9806f5c8
builtwebtough <builtwebtough@gmail.com>
2023-09-29 10:47:23 -0700

    changed account and contact to html modified index and main.php to reflect

--- main.php
+++ main.php
@@ -2,10 +2,15 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
-
 <?php
-echo "<h2>Hello World!</h2>";
+if(isset($_POST['login'])){ echo "<h2>This came from the login page</h2>";
+}elseif(isset($_POST['signup'])){
+echo "<h2>This came from the contact form</h2>";
+}else{
+echo "<h2>This is where we handle all php post from the account page and the contact form<br>
+we will load various pages from this code.
+</h2>";
+}
 ?> 
 
 </body>
